76a51fff6e6ed0324d4f8e85fe227864131bdcb1
Ranjithkumar s <144662966+RanjithYsi@users.noreply.github.com>
2025-07-20 10:25:21 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("contact-form").addEventListener("submit", function(e) {
-  e.preventDefault();
-  document.getElementById("form-status").textContent = "Sending...";
-  
-  setTimeout(() => {
-    document.getElementById("form-status").textContent = "Message sent successfully!";
-    this.reset();
-  }, 1000);
-});
